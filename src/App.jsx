5d0a5dee6449d54496eca691f396827267b0b4b1
Ferin Sharaf <ferinsharaf20@gmail.com>
2026-04-01 00:13:50 +0530

    sf

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
 import GreetSection from "./components/GreetSection"
 import './App.css'
+import { useEffect, useState } from "react"
 
 
 
 function App() {
+  const [scrollProgress, setScrollProgress] = useState(0)
+  // console.log(scrollProgress)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const progress = window.scrollY / window.innerHeight
+      setScrollProgress(progress)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (<>
-    <GreetSection />
+    <GreetSection scrollProgress={scrollProgress}/>
+    <div style={{height:"200vh"}}></div>
   </>)
 }
 
--- src/components/GreetSection.jsx
+++ src/components/GreetSection.jsx
@@ -1,47 +1,70 @@
 import { useState, useEffect } from 'react';
+import { keyframes, styled } from "styled-components"
 import scrollDownLogo from '../assets/down-arrow.png'
 import '../styles/GreetSection.css'
 
-export default function GreetSection () {
-  const greetings = ["HI,", "Im", "Ferin", "Sharaf"];
-  const [visibleCount, setVisibleCount] = useState(0);
-  const [showScroller, setShowScroller] = useState(true);
-  console.log(showScroller)
-
-  useEffect(() => {    
-    const animDur = 8;
-
-    const interval = setInterval(() => {
-      setVisibleCount((prev) => {
-        if (prev === greetings.length) {
-          clearInterval(interval);
-          return prev;
+export default function GreetSection ({ scrollProgress }) {
+    // const animate = scrollProgress > 0.3
+    
+    const rotate = keyframes`
+        from {
+            transform: rotate(0deg);
         }
-        return prev + 1;
-      })
-    }, animDur);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [])
+        to {
+            transform: rotate(360deg);
+        }
+    `;
+
+    const scale = Math.max(1 - scrollProgress * 0.8, 0.2);
+    const opacity = Math.max(1 - scrollProgress * 1.5, 0);
+    // console.log("s: ", scale)
+
+    const greetings = ["HI,", "Im", "Ferin", "Sharaf"];
+    const [visibleCount, setVisibleCount] = useState(0);
+    
+    useEffect(() => {    
+        const animDur = 8;
+
+        const interval = setInterval(() => {
+        setVisibleCount((prev) => {
+            if (prev === greetings.length) {
+            clearInterval(interval);
+            return prev;
+            }
+            return prev + 1;
+        })
+        }, animDur);
+
+        return () => {
+        clearInterval(interval);
+        };
+    }, [])
 
 
-  return (
-    <section id="greeter-section">
-      <section id="greeter-text">
-        {greetings.slice(0, visibleCount).map((text, index) => (
-          <section key={index} className="greet-element">
-            {text}
-          </section>
-        ))}
-      </section>
-      <section id='greeter-scroller'>
-        <p className="greet-scroll-text">
-          Scroll Down
-        </p>
-        <img src={scrollDownLogo} alt="scroll down logo" />
-      </section>
-    </section>
-  )
+    return (
+        <section id="greeter-section">
+        <section id="greeter-text">
+            {greetings.slice(0, visibleCount).map((text, index) => (
+            <section key={index} className="greet-element">
+                {text}
+            </section>
+            ))}
+        </section>
+        <section 
+            id='greeter-scroller'
+            style={{ 
+                transform: `scale(${scale})`,
+                opacity: opacity,
+                
+            }}
+        >
+            <p className="greet-scroll-text">
+            Scroll Down
+            </p>
+            <img src={scrollDownLogo} alt="scroll down logo" />
+        </section>
+        {/* <GreetScrollbar /> */}
+        </section>
+    )
 }
